6e1b3140ece1c9f5fe50ee3cf69868ef380f9642
Alejandro-Estrada-1 <alejandro.sanchez.estrada@ciencias.unam.mx>
2023-10-02 20:57:44 -0600

    hello

--- src/hello.py
+++ src/hello.py
@@ -9,9 +9,13 @@ def index():
 def home():
     return render_template('home.html')
 
-@app.route('/about/')
-def about():
-    return "render_template('about.html')"
+@app.route('/pagina1.html/')
+def pag1():
+    return render_template('pagina1.html')
+
+@app.route('/pagina2.html/')
+def pag2():
+    return render_template('pagina2.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
