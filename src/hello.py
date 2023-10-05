aa5b33bd3dc30842c14e13e3e5fd10a5844fa1c3
Alejandro-Estrada-1 <alejandro.sanchez.estrada@ciencias.unam.mx>
2023-10-05 00:09:54 -0600

    js1

--- src/hello.py
+++ src/hello.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import requests
 app = Flask(__name__)
 
 @app.route('/index/')
@@ -16,6 +17,40 @@ def city():
 @app.route('/ticket.html/')
 def ticket():
     return render_template('ticket.html')
+
+# Ruta para buscar datos climáticos por nombre de ciudad
+@app.route('/buscar_ciudad')
+def searchWeatherWith_NameOfCity():
+    city_name = request.args.get('city')  # Obtener el nombre de la ciudad desde la solicitud
+    weather_data = get_weather_by_city_name(city_name)  # Llamar a la función para obtener el clima
+    return jsonify(weather_data)
+
+# Función para obtener datos climáticos de OpenWeather API
+def get_weather_by_city_name(city_name):
+    api_key = "&appid=155505a47faf9082a7ee3d45f7b1ea0b&units=metric"
+    base_url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "q": city_name,
+        "appid": api_key,
+        "units": "metric"  
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        data = response.json()
+
+        weather_data = {
+            "name": data['name'],
+            "temp": data['main']['temp'],
+            "humidity": data['main']['humidity'],
+            "wind": data['wind']['speed']
+        }
+
+        return weather_data
+
+    except Exception as e:
+        print("Error al obtener datos climáticos:", e)
+        return {"error": "No se pudo obtener la información del clima"}
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,26 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector(".search");
-    const input = document.querySelector("input");
-    const weatherCard = document.getElementById("weather-card");
+    const getWeatherButton = document.getElementById("get-weather-button");
 
-    form.addEventListener("submit", async function (e) {
+    getWeatherButton.addEventListener("click", function (e) {
         e.preventDefault();
 
-        const cityName = input.value;
-
-        try {
-            const response = await fetch(`/buscar_ciudad?city=${cityName}`);
-            const data = await response.json();
-
-            // Actualizar la "CARD 2" con los datos obtenidos de la API
-            weatherCard.querySelector(".temp").textContent = `${data.temp}°C`;
-            weatherCard.querySelector(".card-category").textContent = data.name;
-            weatherCard.querySelector(".humidity").textContent = `${data.humidity}%`;
-            weatherCard.querySelector(".wind").textContent = `${data.wind} km/h`;
-
-        } catch (error) {
-            console.error("Error al obtener datos del clima:", error);
-            alert("No se pudo obtener la información del clima para la ciudad ingresada.");
-        }
+        const cityName = input.value; 
+        updateWeather(cityName); 
     });
+
+    // Función para obtener y mostrar el clima
+    function updateWeather(cityName) {
+        const weatherCard = document.getElementById("weather-card");
+
+        // Realizar una solicitud AJAX a la función Python
+        fetch(`/buscar_ciudad?city=${cityName}`)
+            .then((response) => response.json())
+            .then((data) => {
+                // Actualizar la "CARD 2" con los datos obtenidos de la API
+                weatherCard.querySelector(".temp").textContent = `${data.temp}°C`;
+                weatherCard.querySelector(".card-category").textContent = data.name;
+                weatherCard.querySelector(".humidity").textContent = `${data.humidity}%`;
+                weatherCard.querySelector(".wind").textContent = `${data.wind} km/h`;
+            })
+            .catch((error) => {
+                console.error("Error al obtener datos del clima:", error);
+                alert("No se pudo obtener la información del clima para la ciudad ingresada.");
+            });
+    }
 });
