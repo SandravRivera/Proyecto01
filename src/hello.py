ae9795c315972324b4173395683b2b23afdf1940
Alejandro-Estrada-1 <alejandro.sanchez.estrada@ciencias.unam.mx>
2023-10-05 23:22:30 -0600

    uys

--- src/hello.py
+++ src/hello.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import requests
+from methods import start, searchWeatherWith_NameOfCity 
 
 app = Flask(__name__)
 
@@ -20,41 +21,10 @@ def ticket():
     return render_template('ticket.html')
 
 @app.route('/buscar_ciudad')
-def searchWeatherWith_NameOfCity():
-    city_name = request.args.get('city') 
-    weather_data = get_weather_by_city_name(city_name)  
+def search_weather_by_city_name():
+    city_name = request.args.get('city')
+    weather_data = searchWeatherWith_NameOfCity(city_name) 
     return jsonify(weather_data)
-
-# Función para obtener datos climáticos de OpenWeather API
-def get_weather_by_city_name(city_name):
-    api_key = "&appid=155505a47faf9082a7ee3d45f7b1ea0b&units=metric"
-    base_url = "https://api.openweathermap.org/data/2.5/weather"
-    params = {
-        "q": city_name,
-        "appid": api_key,
-        "units": "metric"  
-    }
-
-    try:
-        response = requests.get(base_url, params=params)
-        data = response.json()
-
-        if 'name' in data and 'main' in data and 'temp' in data['main'] and 'humidity' in data['main'] and 'wind' in data:
-            weather_data = {
-                "name": data['name'],
-                "temp": data['main']['temp'],
-                "humidity": data['main']['humidity'],
-                "wind": data['wind']['speed']
-            }
-
-            return weather_data
-        else:
-            print("Datos faltantes en la respuesta de la API de OpenWeather")
-            return {"error": "Datos climáticos incompletos"}
-
-    except Exception as e:
-        print("Error al obtener datos climáticos:", e)
-        return {"error": "No se pudo obtener la información del clima"}
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,57 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
     const getWeatherButton = document.getElementById("get-weather-button");
-    let weatherCache = {};
+    const cityNameInput = document.getElementById("city-name-input");
+    const weatherCard = document.getElementById("weather-card");
 
     getWeatherButton.addEventListener("click", function (e) {
         e.preventDefault();
 
-        const cityNameInput = document.getElementById("city-name-input");
         const cityName = cityNameInput.value;
 
-        // Comprobar si los datos ya están en caché
-        const cachedWeatherData = getCachedWeatherData(cityName);
-        if (cachedWeatherData) {
-            updateWeatherCard(cachedWeatherData);
-        } else {
-            // Realizar una solicitud AJAX a la función Python
-            fetch(`/buscar_ciudad?city=${cityName}`)
-                .then((response) => response.json())
-                .then((data) => {
-                    if (data && data.name && data.temp && data.humidity && data.wind) {
-                        // Almacenar los datos en caché
-                        cacheWeatherData(cityName, data);
-
-                        // Actualizar la "CARD 2" con los datos obtenidos de la API
-                        updateWeatherCard(data);
-                    } else {
-                        console.error("Datos faltantes en la respuesta de la API de OpenWeather");
-                        alert("Los datos climáticos no están disponibles para la ciudad ingresada.");
-                    }
-                })
-                .catch((error) => {
-                    console.error("Error al obtener datos del clima:", error);
-                    alert("No se pudo obtener la información del clima para la ciudad ingresada.");
-                });
-        }
+        fetch(`/searchWeatherWith_NameOfCity?city=${cityName}`)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data && data.weather) {
+                    const weather = data.weather;
+                    // Actualizar la "CARD 2" con los datos obtenidos
+                    weatherCard.querySelector(".temp").textContent = `${weather.temp}°C`;
+                    weatherCard.querySelector(".card-category").textContent = weather.name;
+                    weatherCard.querySelector(".humidity").textContent = `${weather.humidity}%`;
+                    weatherCard.querySelector(".weather").textContent = weather.weather;
+                } else {
+                    console.error("Datos faltantes en la respuesta de la API de OpenWeather");
+                    alert("Los datos climáticos no están disponibles para la ciudad ingresada.");
+                }
+            })
+            .catch((error) => {
+                console.error("Error al obtener datos del clima:", error);
+                alert("No se pudo obtener la información del clima para la ciudad ingresada.");
+            });
     });
-
-    function cacheWeatherData(cityName, data) {
-        // Almacenar los datos en caché en el objeto weatherCache
-        weatherCache[cityName] = data;
-    }
-
-    function getCachedWeatherData(cityName) {
-        // Obtener los datos en caché desde el objeto weatherCache
-        return weatherCache[cityName];
-    }
-
-    function updateWeatherCard(data) {
-        const weatherCard = document.getElementById("weather-card");
-
-        // Actualizar la "CARD 2" con los datos obtenidos de la API
-        weatherCard.querySelector(".temp").textContent = `${data.temp}°C`;
-        weatherCard.querySelector(".card-category").textContent = data.name;
-        weatherCard.querySelector(".humidity").textContent = `${data.humidity}%`;
-        weatherCard.querySelector(".wind").textContent = `${data.wind} km/h`;
-    }
 });
